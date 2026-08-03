47160e84c26bf5c1fb025b5ca106b57130195300
miguel-peters <miguelppeters@gmail.com>
2026-08-03 00:04:41 -0300

    REST API teste para futuros projetos

--- API2.py
+++ API2.py
@@ -1,12 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
-
-
-
 
 class Drink(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -37,3 +35,19 @@ def get_drinks():
 def get_drink(id):
     drink = Drink.query.get_or_404(id)
     return {'name': drink.name, 'description': drink.description}
+
+@app.route('/drinks', methods=['POST'])
+def add_drink():
+    drink = Drink(name=request.json['name'], description=request.json['description'])
+    db.session.add(drink)
+    db.session.commit()
+    return {'id': drink.id}
+
+@app.route('/drinks/<id>', methods=['DELETE'])
+def delete_drink(id):
+    drink = Drink.query.get(id)
+    if drink is None:
+        return {"error": "not found"}
+    db.session.delete(drink)
+    db.session.commit()
+    return {"message": "yeet!"}
